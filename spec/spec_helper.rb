6b70aedc78ef927138f1bd34c2c12456055f4aed
donvnielsen <donvnielsen@gmail.com>
2018-05-28 15:09:18 -0500

    updated build mgr test data location

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,8 +11,6 @@ ENV['RAILS_ENV'] = 'dev'
 require_relative '../config/environment'
 
 SPEC_DATA_DIR = '/home/dvn/Documents/roehl_app_catalog_test_data'
-BUILDMGR_DIR = File.join(SPEC_DATA_DIR,'build_manager')
-# TEST_SLNPRJ_DIR = File.join(SPEC_DATA_DIR)
 TEST_SOLUTION_FILE = File.join(SPEC_DATA_DIR,'spec_test_solution.sln')
 TEST_PROJECT_FILE  = File.join(SPEC_DATA_DIR,'spec_test_project.csproj')
 
