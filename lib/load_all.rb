afd474135375e59f99ba1d2d1314992dec91183a
dvn <donvnielsen@gmail.com>
2018-05-30 22:45:51 -0500

    rebased off of load_tasks

--- lib/load_all.rb
+++ lib/load_all.rb
@@ -1,29 +1,12 @@
-require 'standalone_migrations'
 require_relative '../config/environment'
 
-ENV['RAILS_ENV'] = 'test'
-
-ActiveRecord::Base.establish_connection(
-  adapter: CONFIG_DB['adapter'],
-  database: File.join(ROOT_DIR, CONFIG_DB['database']),
-  logger: LOGGER
-)
-LOGGER.level = Logger::DEBUG
-LOGGER.info('load_all begin')
-
-puts "Start Loading All #{Time.now}"
-
-migration_db = File.join(ROOT_DIR, CONFIG['dbdir'], 'migrate')
-LOGGER.info("Migrations folder : #{migration_db}")
-ActiveRecord::Migration.migrate(migration_db)
+LOGGER.info("load_all begin")
 
 system 'ruby -S load_app_catalog.rb'
-LOGGER.info('load_app_catalog complete')
+LOGGER.info("load_app_catalog complete")
 system 'ruby -S load_solutions.rb'
-LOGGER.info('load_solutions complete')
+LOGGER.info("load_solutions complete")
 system 'ruby -S load_projects.rb'
-LOGGER.info('load_projects complete')
-system 'ruby -S load_tasks.rb'
-LOGGER.info('load_tasks complete')
+LOGGER.info("load_projects complete")
 
-LOGGER.info('load_all end')
+LOGGER.info("load_all end")
--- lib/load_app_catalog.rb
+++ lib/load_app_catalog.rb
@@ -12,20 +12,24 @@ require_relative '../classes/log_formatter/log_server'
 require_relative '../classes/build_manager/build_def'
 require_relative '../app/models/server'
 require_relative '../app/models/roehl_application'
+require_relative '../classes/log_formatter/log_roehl_application'
 require_relative '../app/models/roehl_application_server'
 
 # ActiveRecord::Base.logger = Logger.new(STDERR)
 ActiveRecord::Base.establish_connection(
-    adapter: CONFIG_DB['adapter'],
-    database: File.join(ROOT_DIR, CONFIG_DB['database']),
-    # logger: LOGGER
+  adapter: 'sqlite3',
+  database: File.join(ROOT_DIR, CONFIG_DB['database']),
+  :logger => LOGGER
 )
+LOGGER.level = Logger::DEBUG
+LOGGER.info('Applications, Servers, App Server references')
 
 puts "Start #{Time.now}"
 
 ActiveRecord::Migration.migrate(File.join(ROOT_DIR, CONFIG['dbdir'], 'migrate'))
 
 def propogate_servers(builds)
+  LOGGER.info(__method__.to_s)
   Server.destroy_all
   servers = builds.map {|build| build.server_name }.uniq
 
@@ -40,15 +44,19 @@ def propogate_servers(builds)
   servers.each {|server|
     pb.increment
     next if server.nil? || server.size == 0
-    Server.create(name: server) if Server.find_by_name(server).nil?
+    if Server.find_by_name(server).nil?
+      o = Server.create(name: server)
+      LOGGER.debug(LogServer.msg(o,'Created server')) if LOGGER.debug?
+    end
   }
 
   pb.progress < pb.total ? pb.stop : pb.finish
 end
 
 def propogate_applications(builds)
+  LOGGER.info(__method__.to_s)
   RoehlApplication.destroy_all
-  apps = builds.map {|build| {name: build.app_name, folder: build.folder} }.uniq
+  apps = builds.map {|build| build.app_name }.uniq
 
   pb = ProgressBar.create(
       title: __method__.to_s,
@@ -60,14 +68,18 @@ def propogate_applications(builds)
 
   apps.each {|app|
     pb.increment
-    next if app.nil? || app[:name] == 0
-    RoehlApplication.create(name: app[:name], folder: app[:folder]) if RoehlApplication.find_by_name(app[:name]).nil?
+    next if app.nil? || app.size == 0
+    if RoehlApplication.find_by_name(app).nil?
+      o = RoehlApplication.create(name: app)
+      LOGGER.debug(LogRoehlApplication.msg(o,'Created application')) if LOGGER.debug?
+    end
   }
 
   pb.progress < pb.total ? pb.stop : pb.finish
 end
 
 def propogate_references(builds)
+  LOGGER.info(__method__.to_s)
   pb = ProgressBar.create(
       title: __method__.to_s,
       total: builds.count,
@@ -92,6 +104,8 @@ def propogate_references(builds)
   }
 end
 
+LOGGER.info('Begin Applications, Servers, App Server References')
+
 # load builds with nokogiri
 builds = nil
 fname = File.join(CONFIG['datadir'], 'build_manager', 'BuildManager.xml')
@@ -113,4 +127,4 @@ propogate_servers(builds)
 propogate_applications(builds)
 propogate_references(builds)
 
-puts "End #{Time.now}"
+LOGGER.info('End Applications, Servers, App Server References')
